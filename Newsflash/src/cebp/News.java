3866ac71b1dbe425d4ce9691d7a6f89caba015d0
AMHategan <49283426+AMHategan@users.noreply.github.com>
2020-12-15 14:04:29 +0200

    updated News

--- Newsflash/src/cebp/News.java
+++ Newsflash/src/cebp/News.java
@@ -2,12 +2,8 @@ package cebp;
 
 import java.util.*;
 
-<<<<<<< HEAD
-public class News {
-=======
 public class News 
 {
->>>>>>> branch 'master' of https://github.com/MCsef/CEBPproj2.git
 	private int id;
 	private String title;
 	private String category;
@@ -15,14 +11,9 @@ public class News
 	private String author;
 	private Date publishDate;
 	private Date modifyDate;
-<<<<<<< HEAD
 	
 	public News(int id, String title, String category, String content, String author, Date publishDate, Date modifyDate) {
 		this.id = id;
-=======
-	public News(String title, String category, String content, String author, Date publishDate, Date modifyDate) 
-	{
->>>>>>> branch 'master' of https://github.com/MCsef/CEBPproj2.git
 		this.title=title;
 		this.category=category;
 		this.content=content;
@@ -30,28 +21,14 @@ public class News
 		this.publishDate=publishDate;
 		this.modifyDate=modifyDate;
 	}
-<<<<<<< HEAD
 	
 	public int getID() {
 		return id;
-=======
-	public String getTitle() 
-	{
-		return title;
->>>>>>> branch 'master' of https://github.com/MCsef/CEBPproj2.git
-	}
-<<<<<<< HEAD
+   }
 	
 	public String getTitle() {
 		return title;
-=======
-	public String getCategory() 
-	{
-		return category;
->>>>>>> branch 'master' of https://github.com/MCsef/CEBPproj2.git
-	}
-<<<<<<< HEAD
-	
+
 	public String getCategory() {
 		return category;
 	}
@@ -100,54 +77,3 @@ public class News
 		this.modifyDate = modifyDate;
 	}
 }
-=======
-	public String getContent() 
-	{
-		return content;
-	}
-	public String getAuthor() 
-	{
-		return author;
-	}
-	public int getID()
-	{
-		return id;
-	}
-	public Date getPublishDate()
-	{
-		return publishDate;
-	}
-	public Date getModifyDate()
-	{
-		return modifyDate;
-	}
-	public void setTitle(String title) 
-	{
-		this.title = title;
-	}
-	public void setCategory(String category)
-	{
-		this.category=category;
-	}
-	public void setContent(String content)
-	{
-		this.content = content;
-	}
-	public void setAuthor(String author) 
-	{
-		this.author = author;
-	}
-	public void setID(int id)
-	{
-		this.id = id;
-	}
-	public void setPublishDate(Date publishDate) 
-	{
-		this.publishDate = publishDate;
-	}
-	public void setModifyDate(Date modifyDate) 
-	{
-		this.modifyDate = modifyDate;
-	}
-}
->>>>>>> branch 'master' of https://github.com/MCsef/CEBPproj2.git
